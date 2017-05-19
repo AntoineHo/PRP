fd3f9b984f03f1f497df37426887f701eef6260b
AntoineHo <antoine.houtain@gmail.com>
2017-05-19 10:48:16 +0200

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -32,14 +32,14 @@ class Runner:
         
             # Gathers the fastqc scripts directory path
             fastqc_scripts_dir = os.path.join(self.filehandler.dir, "fastqc_scripts")
-            print(fastqc_scripts_dir)
+
             # Gathers a list of files from the fastqc scripts directory
             scan = os.listdir(fastqc_scripts_dir)
             # For each file in fastqc_scripts_dir
             for script in scan:
                 # Sets the path
                 path = os.path.join(fastqc_scripts_dir, script)
-                print(path)
+
                 # Appends the script path to script_files list
                 script_files.append(path)
                 # Updates info dictionary in the filehandler
